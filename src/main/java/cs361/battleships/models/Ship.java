d538364af68aba49a37ffc682941765f1ee1d6d5
Caius Brindescu <caius.brindescu@gmail.com>
2018-09-27 11:18:22 -0700

    Sprint 1 started code

---
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -0,0 +1,19 @@
+package cs361.battleships.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Ship {
+
+	@JsonProperty private List<Square> occupiedSquares;
+	
+	public Ship(String kind) {
+		//TODO implement
+	}
+
+	public List<Square> getOccupiedSquares() {
+		//TODO implement
+		return null;
+	}
+}
