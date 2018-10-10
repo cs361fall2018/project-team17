05c368a35164f7604827e18e5b8c095c486803f3
wilmotha <wilmtoha@oregonstate.edu>
2018-10-10 15:03:27 -0700

    MAde the get results function, yet to test because I need to make the square class first but will come back to it

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -1,31 +1,73 @@
 package cs361.battleships.models;
+package cs361.battleship.models.Square
 
 public class Result {
 
+	//Private
+	private Square location; //row colm occupied hit
+ 	private Ship currentShip;
+	private int shipCount;
+
+	//Constructors
+	Result(Square square) {
+		location = square;
+		currentShip = new Ship(NULL);
+		shipCount = 3;
+	}
+	Result(Square square, Ship ship){
+		System.out.println("Constructor Called");
+		location = square;
+		currentShip = ship;
+		shipCount = 3;
+	}
+
+	//public
 	public AtackStatus getResult() {
-		//TODO implement
-		return null;
+		location.setHit(1);
+		if ( location.getOccupied() ) {
+			int check_sunk = 0;
+			for(int i = 0; i < currentShip.getLength(); i++){
+				Square shipLocation = currentShip.getOccupiedSquares();
+				if(location.getColumn() == shipLocation[i].getColm() && location.getRow() == shipLocation[i].getRow()){
+					if(shipLocation.getOccupied() == 1){
+						check_sunk++;
+					}
+				}
+			}
+			if(check_sunk == currentShip.getLength()){
+				System.out.println("Sunk Boi");
+				shipCount--;
+				if(shipCount == 0){
+					return AtackStatus.SURRENDER;
+				}else {
+					return AtackStatus.SUNK;
+				}
+			}else{
+				return AtackStatus.HIT;
+			}
+		}
+		return AtackStatus.MISS;
 	}
 
 	public void setResult(AtackStatus result) {
-		//TODO implement
+//		currentStatus = result;
 	}
 
 	public Ship getShip() {
-		//TODO implement
-		return null;
+
+		return currentShip;
 	}
 
 	public void setShip(Ship ship) {
-		//TODO implement
+		currentShip = ship;
 	}
 
 	public Square getLocation() {
-		//TODO implement
-		return null;
+
+		return location;
 	}
 
 	public void setLocation(Square square) {
-		//TODO implement
+		location = square;
 	}
 }
---
+++ src/test/java/cs361/battleships/models/ResultTest.java
@@ -0,0 +1,20 @@
+package cs361.battleships.models;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ResultTest {
+
+    @Before
+    public void setUpResults() {
+//        Result gameResult = new Result();
+    }
+
+    @Test
+    public void testResults() {
+        //Result gameResult = new Result();
+//        System.out.println(gameResult.getResult());
+        //assertEquals(AtackStatus.INVALID, gameResult.getResult());
+    }
+}
