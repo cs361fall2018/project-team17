06dc5393c18e0502c155790300952199343eb82e
Arnav Bhutani <bhutania@oregonstate.edu>
2018-10-12 13:54:54 -0700

    Test stuff

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -15,28 +15,29 @@ public class BoardTest {
     @Test
     public void testInvalidPlacement() {
         Board board = new Board();
-        assertFalse(board.placeShip(new Ship("Minesweeper", 2), 11, 'C', true));    //X outside of bounds
-        assertFalse(board.placeShip(new Ship("Destroyer", 3), 3, 'K', true));    //Y outside of bounds
-        assertFalse(board.placeShip(new Ship("Battleship", 4), 11, 'Z', true));    //X and Y outside of bounds
-        assertFalse(board.placeShip(new Ship("Destroyer", 3), 7, 'A', true));      //X length overflows outside the board
-        assertFalse(board.placeShip(new Ship("Battleship", 4), 2, 'H', false));     //Y length overflows outside the board
+        assertFalse(board.placeShip(new Ship("MINESWEEPER", 2), 11, 'C', true));    //X outside of bounds
+        assertFalse(board.placeShip(new Ship("DESTROYER", 3), 3, 'K', true));    //Y outside of bounds
+        assertFalse(board.placeShip(new Ship("BATTLESHIP", 4), 11, 'Z', true));    //X and Y outside of bounds
+        assertFalse(board.placeShip(new Ship("DESTROYER", 3), 8, 'A', false));      //X length overflows outside the board
+        assertFalse(board.placeShip(new Ship("BATTLESHIP", 4), 2, 'H', true));     //Y length overflows outside the board
     }
 
     @Test
     public void testPlaceShip(){
         Board board = new Board();
-        assertTrue(board.placeShip(new Ship("Destroyer", 3), 6, 'C', true));       //Valid ship placement
+        assertTrue(board.placeShip(new Ship("DESTROYER", 3), 6, 'C', true));       //Valid ship placement
+        assertTrue(board.placeShip(new Ship("MINESWEEPER", 2), 8, 'J', false));          //Valid ship placement
     }
 
     @Test
     public void testAttack(){
         Board board1 = new Board();
-        board1.placeShip(new Ship("Destroyer", 3),6, 'E', true);
+        board1.placeShip(new Ship("DESTROYER", 3),6, 'E', true);
         assertSame(board1.attack(6, 'E').getResult(), AtackStatus.HIT);
         assertSame(board1.attack(11, 'A').getResult(), AtackStatus.INVALID);
         assertSame(board1.attack(1, 'A').getResult(), AtackStatus.MISS);
-        assertSame(board1.attack(7, 'E').getResult(), AtackStatus.HIT);
-        assertSame(board1.attack(8, 'E').getResult(), AtackStatus.SURRENDER);
+        assertSame(board1.attack(6, 'F').getResult(), AtackStatus.HIT);
+        assertSame(board1.attack(6, 'G').getResult(), AtackStatus.SURRENDER);
         assertSame(board1.attack(9, 'E').getResult(), AtackStatus.MISS);
     }
 }
