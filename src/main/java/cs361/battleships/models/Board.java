735e3f198b8bc6da3496fcbfa2907960681162a4
Arnav Bhutani <bhutania@oregonstate.edu>
2018-10-11 21:29:29 -0700

    Completed constructor and two other functions

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -5,16 +5,21 @@ import java.util.List;
 
 public class Board {
 
-    private List<Ship> ships;
-    private List<Result> results;
+    @jsonProperty private List<Ship> ships;
+    @jsonProperty private List<list<Result> results;
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Board() {
-		// TODO Implement
         ships = new ArrayList<>();
-        results = new ArrayList<>();
+        results = new ArrayList<List<>>();
+        for (int i = 0; i < 10; i++) {
+        	List<Square> row = new ArrayList<Result>();
+			for (int j = 0; j < 10; j++) {
+				row.add(new Result(new Square(i, j, false)));
+			}
+		}
 	}
 
 	/*
@@ -29,16 +34,25 @@ public class Board {
             return false;
         else if ( y + ship.getLength()  > 'J' && isVertical)
             return false;
-        else
-            return ship.setOccupiedSquares(x, y, isVertical);
+        else {
+        	boolean returnVar;
+        	returnVar = ship.setOccupiedSquares(x, y, isVertical);
+        	List<Square> temp = ship.getOccupiedSquares();
+        	for (int i = 0; i < temp.size(); i++)
+			{
+				results.get(temp.getRow()).get(temp.getColumn()).setShip(ship);
+			}
+			return returnVar;
+		}
 	}
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Result attack(int x, char y) {
-		//TODO Implement
-		return null;
+		int ycon = y-48;
+		results.get(x).get(ycon).getResult(ships.size());
+		return results.get(x).get(ycon);
 	}
 
 	public List<Ship> getShips() {
