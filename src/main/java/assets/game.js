ade9ecf96877f9a70ee734f8f207c4b0bd755633
gs291 <sanchez.gregory886@gmail.com>
2018-10-25 01:22:32 -0700

    Add working CPU attacks

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -42,16 +42,16 @@ function makeGrid(table, isPlayer) {
 function markHits(board, elementId, surrenderText) {
     board.attacks.forEach((attack) => {
         let className;
-        if (attack.result === "MISS")
-            className = "miss";
-        else if (attack.result === "HIT")
-            className = "hit";
-        else if (attack.result === "SUNK")
-            className = "hit"
-        else if (attack.result === "SURRENDER")
-            alert(surrenderText);// move this and make something cooler
-        document.getElementById(elementId).rows[attack.location.row-1].cells[attack.location.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add(className);
-    });
+    if (attack.result === "MISS")
+        className = "miss";
+    else if (attack.result === "HIT")
+        className = "hit";
+    else if (attack.result === "SUNK")
+        className = "hit"
+    else if (attack.result === "SURRENDER")
+        alert(surrenderText);// move this and make something cooler
+    document.getElementById(elementId).rows[attack.location.row-1].cells[attack.location.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add(className);
+});
 }
 
 function redrawGrid() {
