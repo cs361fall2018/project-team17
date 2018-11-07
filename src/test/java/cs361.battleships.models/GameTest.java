172b9bfa4e0ee16007f2a9f3d4cdbbfcbd098eb8
Austin Wilmoth <wilmotha@oregonstate.edu>
2018-11-07 11:24:57 -0800

    added

--- src/test/java/cs361.battleships.models/GameTest.java
+++ src/test/java/cs361.battleships.models/GameTest.java
@@ -12,10 +12,7 @@ public class GameTest {
     @Test
     public void testAttack(){
         Game test = new Game();
-        boolean mine = test.placeShip((new Ship("MINESWEEPER")), 3, 'D', true);
-        boolean dest = test.placeShip((new Ship("DESTROYER")), 6, 'G', false);
-        boolean batt = test.placeShip((new Ship("BATTLESHIP")), 9, 'B', false);
-
+        
         assertEquals(true, test.attack(3,'H'));
         assertEquals(true, test.attack(4, 'G'));
         assertEquals(false, test.attack(4, 'G'));
